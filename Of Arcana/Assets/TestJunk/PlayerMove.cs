08822933de4084c2dbcc5fa531e2a8857d0589c3
Raphael Mastel III <s179135@students.aie.edu.au>
2018-05-30 15:17:07 -0700

    It will be done!

---
+++ Of Arcana/Assets/TestJunk/PlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float moveMod = 3;
+    public float jumpMod = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody>().AddForce(new Vector3(Input.GetAxis("Horizontal") * moveMod, 0, Input.GetAxis("Vertical") * moveMod));
+        GetComponent<Rigidbody>().AddForce(new Vector3(0, Input.GetAxis("Jump") * jumpMod, 0));
+	}
+}
