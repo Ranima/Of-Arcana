4df3752971f8e40b6219ff5fbf7e009dc00aaf25
Raphael Mastel III <s179135@students.aie.edu.au>
2018-06-01 17:52:08 -0700

    Ive just gotta write my own physics

--- Of Arcana/Assets/TestJunk/PlayerMove.cs
+++ Of Arcana/Assets/TestJunk/PlayerMove.cs
@@ -7,12 +7,18 @@ public class PlayerMove : MonoBehaviour {
     public float moveMod = 3;
     public float jumpMod = 1;
     public float stopMod = 0.8f;
+    public float fallSpeed = 10;
+    public float surface = 60;
 
     private Vector3 move;
+    private bool grounded;
+    private float maxFallSpeed;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        maxFallSpeed = fallSpeed;
+        fallSpeed = 0;
+        grounded = false;
 	}
 	
 	// Update is called once per frame
@@ -25,11 +31,42 @@ public class PlayerMove : MonoBehaviour {
     {
         move.x = Input.GetAxis("Horizontal") * moveMod;
         move.z = Input.GetAxis("Vertical") * moveMod;
-        
-        GetComponent<Rigidbody>().AddForce(new Vector3(0, Input.GetAxis("Jump") * jumpMod, 0));
 
         gameObject.transform.Translate(move);
+        Gravity(grounded);
 
         move = move * stopMod;
+    }
+
+    void Gravity(bool t)
+    {
+        if(!t)
+        {
+            fallSpeed += maxFallSpeed * Time.deltaTime;
+            if(fallSpeed > maxFallSpeed)
+            {
+                fallSpeed = maxFallSpeed;
+            }
+            gameObject.transform.Translate(0, -fallSpeed, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray();
+        ray.origin = transform.position;
+        ray.direction = -Vector3.up;
+
+        if(Physics.Raycast(ray, out hit) && hit.normal.magnitude < surface &&col.collider == hit.collider)
+        {
+            fallSpeed = 0;
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit()
+    {
+        grounded = false;
     }
 }
