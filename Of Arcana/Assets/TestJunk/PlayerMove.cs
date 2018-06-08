f3a66369b6ed0b90381912018c9b444a87c1e617
Raphael Mastel III <s179135@students.aie.edu.au>
2018-06-07 17:48:13 -0700

    Gravity is working

--- Of Arcana/Assets/TestJunk/PlayerMove.cs
+++ Of Arcana/Assets/TestJunk/PlayerMove.cs
@@ -2,71 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerMove : MonoBehaviour
+{
 
     public float moveMod = 3;
     public float jumpMod = 1;
     public float stopMod = 0.8f;
     public float fallSpeed = 10;
+    public float fallAcceleration = .8f;
     public float surface = 60;
 
+    private CharacterController cc;
     private Vector3 move;
     private bool grounded;
     private float maxFallSpeed;
 
-	// Use this for initialization
-	void Awake () {
+    // Use this for initialization
+    void Awake()
+    {
+        cc = GetComponent<CharacterController>();
         maxFallSpeed = fallSpeed;
         fallSpeed = 0;
         grounded = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Movement();
-        
-	}
+
+    }
 
     void Movement()
     {
         move.x = Input.GetAxis("Horizontal") * moveMod;
         move.z = Input.GetAxis("Vertical") * moveMod;
 
-        gameObject.transform.Translate(move);
+        //gameObject.transform.Translate(move);
+        Grounded();
         Gravity(grounded);
+        cc.Move(move);
 
         move = move * stopMod;
     }
 
     void Gravity(bool t)
     {
-        if(!t)
+        if (!t)
         {
-            fallSpeed += maxFallSpeed * Time.deltaTime;
-            if(fallSpeed > maxFallSpeed)
+            fallSpeed += Time.deltaTime * fallAcceleration;
+            if (fallSpeed > maxFallSpeed)
             {
                 fallSpeed = maxFallSpeed;
             }
-            gameObject.transform.Translate(0, -fallSpeed, 0);
+            move.y = -fallSpeed;
         }
     }
 
-    void OnCollisionEnter(Collision col)
+    void Grounded()
     {
-        RaycastHit hit;
-        Ray ray = new Ray();
-        ray.origin = transform.position;
-        ray.direction = -Vector3.up;
-
-        if(Physics.Raycast(ray, out hit) && hit.normal.magnitude < surface &&col.collider == hit.collider)
+        if((cc.collisionFlags & CollisionFlags.Below) != 0)
         {
-            fallSpeed = 0;
             grounded = true;
+            fallSpeed = 0;
         }
-    }
-
-    void OnCollisionExit()
-    {
-        grounded = false;
+        else
+        {
+            grounded = false;
+        }
     }
 }
